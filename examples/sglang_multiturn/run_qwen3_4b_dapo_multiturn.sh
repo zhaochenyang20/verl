9babbb21b1206b7a58dc3814c660274adf2d65f8
zhaochenyang20 <zhaochenyang20@gmail.com>
2025-08-07 08:32:19 -0700

    do not agent loop

--- examples/sglang_multiturn/run_qwen3_4b_dapo_multiturn.sh
+++ examples/sglang_multiturn/run_qwen3_4b_dapo_multiturn.sh
@@ -48,7 +48,6 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=8 \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=8 \
     actor_rollout_ref.rollout.name=sglang \
-    actor_rollout_ref.rollout.mode=async \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.80 \
     actor_rollout_ref.rollout.update_weights_bucket_megabytes=512 \
