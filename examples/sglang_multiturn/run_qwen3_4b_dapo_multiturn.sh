9fc59434462b7ddc250c4f72db3d57d3845da9ac
zhaochenyang20 <zhaochenyang20@gmail.com>
2025-08-21 21:48:30 -0700

    upd dapo

--- examples/sglang_multiturn/run_qwen3_4b_dapo_multiturn.sh
+++ examples/sglang_multiturn/run_qwen3_4b_dapo_multiturn.sh
@@ -17,10 +17,10 @@ hf download \
     --repo-type dataset \
     --local-dir $HOME/data/Maxwell-Jia/AIME_2024
 
-# Note that this script is using AgentLoop instead of SGLang Multi-Turn
-# We are concerned that the reward is not actually converge, since the
-# reward of retool is encouraging the model to generate more turns to
-# call more tools. The answers are not actually correct.
+
+# Note that the model (font-info/qwen3-4b-sft) is a fine-tuned one
+# by SGLang RL team. Without SFT on retool, the dapo training won't
+# converge.
 
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
@@ -31,14 +31,14 @@ python3 -m verl.trainer.main_ppo \
     data.return_raw_chat=True \
     data.train_batch_size=32 \
     data.max_prompt_length=2048 \
-    data.max_response_length=16384 \
+    data.max_response_length=5000 \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
     data.custom_cls.path=$PROJECT_DIR/recipe/retool/retool.py \
     data.custom_cls.name=CustomRLHFDataset \
     custom_reward_function.path=$PROJECT_DIR/recipe/retool/retool.py \
     custom_reward_function.name=compute_score \
-    actor_rollout_ref.model.path=Qwen/Qwen3-4B-Instruct-2507 \
+    actor_rollout_ref.model.path=font-info/qwen3-4b-sft \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.use_kl_loss=False \
@@ -47,16 +47,16 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.clip_ratio_high=0.28 \
     actor_rollout_ref.actor.clip_ratio_c=10.0 \
     actor_rollout_ref.actor.optim.lr=1e-6 \
-    actor_rollout_ref.actor.ppo_mini_batch_size=32 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8 \
-    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=32768 \
-    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=8 \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=8 \
+    algorithm.dynamic_filter.enable=False \
+    actor_rollout_ref.actor.use_dynamic_bsz=False \
+    actor_rollout_ref.actor.ppo_mini_batch_size=8 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
+    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=1024 \
     actor_rollout_ref.rollout.name=sglang \
     actor_rollout_ref.rollout.mode=async \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.80 \
-    actor_rollout_ref.rollout.update_weights_bucket_megabytes=512 \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=4 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.85 \
     actor_rollout_ref.rollout.multi_stage_wake_up=True \
     actor_rollout_ref.rollout.multi_turn.enable=True \
     actor_rollout_ref.rollout.multi_turn.max_user_turns=16 \
@@ -66,10 +66,10 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.n=8 \
     actor_rollout_ref.rollout.val_kwargs.top_p=0.6 \
     actor_rollout_ref.rollout.val_kwargs.temperature=1.0 \
-    actor_rollout_ref.rollout.val_kwargs.n=30 \
+    actor_rollout_ref.rollout.val_kwargs.n=1 \
     trainer.logger=['console','wandb'] \
     trainer.project_name=sglang-dapo-multiturn \
-    trainer.experiment_name=qwen3-4b_dapo_multiturn \
+    trainer.experiment_name=qwen2_5-3b_dapo_multiturn \
     trainer.n_gpus_per_node=8 \
     trainer.log_val_generations=20 \
     trainer.val_before_train=True \
