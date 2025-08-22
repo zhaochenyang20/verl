4e0a2c0d2657cca6cc3ef2752039d18f6fb2d928
Zhuorany <yzr1914001753@gmail.com>
2025-08-22 22:10:38 +0000

    updated model name

--- examples/sglang_multiturn/run_qwen3_4b_dapo_multiturn.sh
+++ examples/sglang_multiturn/run_qwen3_4b_dapo_multiturn.sh
@@ -24,7 +24,7 @@ hf download \
 # docker run -it -p 8080:8080 volcengine/sandbox-fusion:server-20250609
 
 # 2. 
-# The model located at font-info/qwen3-4b-sft (https://huggingface.co/font-info/qwen3-4b-sft)
+# The model located at font-info/qwen3-4b-sft-SGLang-RL (https://huggingface.co/font-info/qwen3-4b-sft-SGLang-RL)
 # is a fine-tuned version provided by the SGLang RL team. Without supervised fine-tuning (SFT)
 # on the Retool dataset, Dapo training will not converge.
 
@@ -51,14 +51,14 @@ python3 -m verl.trainer.main_ppo \
     data.return_raw_chat=True \
     data.train_batch_size=32 \
     data.max_prompt_length=2048 \
-    data.max_response_length=5000 \
+    data.max_response_length=16384 \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
     data.custom_cls.path=$PROJECT_DIR/recipe/retool/retool.py \
     data.custom_cls.name=CustomRLHFDataset \
     custom_reward_function.path=$PROJECT_DIR/recipe/retool/retool.py \
     custom_reward_function.name=compute_score \
-    actor_rollout_ref.model.path=font-info/qwen3-4b-sft \
+    actor_rollout_ref.model.path=font-info/qwen3-4b-sft-SGLang-RL \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.use_kl_loss=False \
@@ -75,6 +75,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.mode=async \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=4 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
+    actor_rollout_ref.rollout.update_weights_bucket_megabytes=512 \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.85 \
     actor_rollout_ref.rollout.multi_stage_wake_up=True \
     actor_rollout_ref.rollout.multi_turn.enable=True \
